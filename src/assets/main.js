b7bf5fdd109111868f4afb2b85f0b5834e4b15a9
zjc93 <jimmy93cao@yahoo.com>
2017-08-01 15:03:50 -0700

    transformed array of json objects to html using jquery $.map and ajax

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,19 @@
 $(function() {
+  $.ajax('https://www.codeschool.com/users/zjc93.json', {
+    dataType: 'jsonp',
+    success: function(result){
+      console.log(result);
+      var completedCourses = $.map(result.courses.completed, function(completed, i){
+        var courses = $('<div class="course"></div>');
+        courses.append($('<h3>' + completed.title + '</h3>'));
+        courses.append($('<img src="' + completed.badge + '"></img>'));
+        courses.append($('<a href="' + completed.url + '" target="_blank" class="btn btn-primary">See Course</a>'));
+        return courses;
+      });
+      console.log(completedCourses);
+      $('#badges').append(completedCourses);
+      console.log($('#badges'));
 
-  // your code will go here
-
+    }
+  });
 });
